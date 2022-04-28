0cfdf51ac38dddfd7e4a03c2352e5e6b87a19406
Brian Linville <brian.j.linville@gmail.com>
2022-04-28 14:31:02 -0700

    fix

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,65 +1,20 @@
-// Requires
-const { getMLnames, getMLlastNames, getMLpronoun, getMLtown, getMLfamily, getMLraised, getMLambition, getMLcondition, getMLresult, getMLage, getMLdeath, getMLadvice } = require('./arrays');
-const express = require('express');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
-const app = express();
+const path = require('path');
+const { getStoryAPI, getStoryP1, getStoryP2, getStoryAdvice, getCharCodes } = require('../models/madlibs');
+const { app } = require('../app');
 
-
-// Convert text string into ascii values per character and turn numbers into one int.
-function getCharCodes(nameString, age){
-    let nameSum = 0;
-    let base = 0;
-    let baseAdd = 0;
-    let slength = nameString.length;
-    if (slength > 10) {
-        slength = 10;
-    }
-    for(let i = 0; i < slength; i++){
-        let code = nameString.charCodeAt(i);
-        base = Math.pow(10, i);
-        baseAdd = code * base;
-        nameSum = nameSum + baseAdd;
-    }
-    return nameSum * age;
-}
-
-
-// Generate MadLib
-/* In a past life <input name> was born <name> <last name>.
-** <name> was born in <town> to a family of <family>.
-** Raised by <raised>, <name>'s life ambition was to
-** <ambition>, <condition> <result>
-**
-** Sadly, <name> passed away at the age of <old age> <cause of death>.  On
-** <his / her> tombstone is written the sage advice:
-**
-** <adice from outside API> */
-function getStoryAPI (v, inputName) {
-    let story = "In a past life, " + inputName + " was born " + getMLnames(v) + " " + getMLlastNames(v) + ".  ";
-    story = story + getMLnames(v) +  " was born in " + getMLtown(v) + " to a family of " + getMLfamily(v) + ".  ";
-    story = story + "Raised by " + getMLraised(v) + ", " + getMLnames(v) + "'s life ambition was to ";
-    story = story + getMLambition(v) + ", " + getMLcondition(v) + " " + getMLresult(v) + ".  Sadly, ";
-    story = story + getMLnames(v) +  " passed away at the age of " + getMLage(v) + " " + getMLdeath(v) + ".  On ";
-    story = story + getMLpronoun(v) + " tombstone is written the sage advice: " + getMLadvice(v);
-    return story;
-}
-function getStoryP1 (v, inputName) {
-    let story = "In a past life, " + inputName + " was born " + getMLnames(v) + " " + getMLlastNames(v) + ".  ";
-    story = story + getMLnames(v) +  " was born in " + getMLtown(v) + " to a family of " + getMLfamily(v) + ".  ";
-    story = story + "Raised by " + getMLraised(v) + ", " + getMLnames(v) + "'s life ambition was to ";
-    story = story + getMLambition(v) + ", " + getMLcondition(v) + " " + getMLresult(v) + ".";
-    return story;
-}
-
-function getStoryP2 (v, inputName) {
-    let story = "Sadly, "  + getMLnames(v) +  " passed away at the age of " + getMLage(v) + " " + getMLdeath(v) + ".  On ";
-    story = story + getMLpronoun(v) + " tombstone is written the sage advice:";
-    return story;
-}
-
-function getStoryAdvice (v, inputName) {
-    return getMLadvice(v);
-}
-
-module.exports = { getStoryAPI, getStoryP1, getStoryP2, getStoryAdvice, getCharCodes };
+module.exports = function(app) {
+    app.get('/', function(req, res){
+        res.render('index');
+    });
+    app.post('/api', function(req, res){
+        console.log(req.body);
+        res.send(getStoryAPI(getCharCodes(req.body.inputName, req.body.inputAge), req.body.inputName));
+    });
+    app.post('/results', function(req, res){
+        const StoryP1 = getStoryP1(getCharCodes(req.body.inputName, req.body.inputAge), req.body.inputName);
+        const StoryP2 = getStoryP2(getCharCodes(req.body.inputName, req.body.inputAge), req.body.inputName);
+        const StoryP3 = getStoryAdvice(getCharCodes(req.body.inputName, req.body.inputAge), req.body.inputName);
+        res.render('results', {p1: StoryP1, p2: StoryP2, p3: StoryP3});
+        console.log('Got body:', req.body);
+    });
+};
